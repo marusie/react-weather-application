4fb688048a1221d31a804002445fdf28e1e3fec6
Mariana Podushkina <mariana.rudaya@gmail.com>
2023-02-05 16:54:38 +0100

    changed visibility

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">weatherddv</div>;
+  return <div className="App">weather</div>;
 }
 
 export default App;
